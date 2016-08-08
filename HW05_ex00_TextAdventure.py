6939fb00f6d12b98a65c7bf7dbb84b3d0f50ed49
ghvik <vreddy@ischool.berkeley.edu>
2016-08-08 16:44:17 -0700

    Finished text adventure

--- HW05_ex00_TextAdventure.py
+++ HW05_ex00_TextAdventure.py
@@ -6,25 +6,24 @@ from sys import exit
 
 # Body
 
-
-def infinite_stairway_room(count=0):
-    print("You walk through the door to see a dimly lit hallway.")
+def infinite_stairway_room(name,count=0):
+    print("{} walks through the door to see a dimly lit hallway.".format(name))
     print("At the end of the hallway is a", count * 'long ', 'staircase going towards some light')
     next = input("> ")
     
     # infinite stairs option
     if next == "take stairs":
-        print('You take the stairs')
+        print('{} take the stairs'.format(name))
         if (count > 0):
-            print("but you're not happy about it")
-        infinite_stairway_room(count + 1)
+            print("but {} is not happy about it".format(name))
+        infinite_stairway_room(name, count + 1)
     # option 2 == ?????
     if next == option_2:
         pass
 
 
-def gold_room():
-    print("This room is full of gold.  How much do you take?")
+def gold_room(name):
+    print("This room is full of gold.  How much do {} take?".format(name))
 
     next = input("> ")
     if "0" in next or "1" in next:
@@ -33,48 +32,48 @@ def gold_room():
         dead("Man, learn to type a number.")
 
     if how_much < 50:
-        print("Nice, you're not greedy, you win!")
+        print("Nice, {} is not greedy, {} wins!".format(name, name))
         exit(0)
     else:
-        dead("You greedy goose!")
+        dead("{} greedy goose!".format(name))
 
 
-def bear_room():
+def bear_room(name):
     print("There is a bear here.")
     print("The bear has a bunch of honey.")
     print("The fat bear is in front of another door.")
-    print("How are you going to move the bear?")
+    print("How is {} going to move the bear?".format(name))
     bear_moved = False
 
     while True:
         next = input("> ")
 
-        if next == "take honey":
-            dead("The bear looks at you then slaps your face off.")
+        if next == "take" or next == "honey":
+            dead("The bear looks at {} then slaps {} face off.".format(name, name + "\'s"))
         elif next == "taunt bear" and not bear_moved:
-            print("The bear has moved from the door. You can go through it now.")
+            print("The bear has moved from the door. {} can go through it now.".format(name))
             bear_moved = True
-        elif next == "taunt bear" and bear_moved:
+        elif next == "taunt" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
-        elif next == "open door" and bear_moved:
-            gold_room()
+        elif next == "open" or next == "door" and bear_moved:
+            gold_room(name)
         else:
             print("I got no idea what that means.")
 
 
-def cthulhu_room():
-    print("Here you see the great evil Cthulhu.")
-    print("He, it, whatever stares at you and you go insane.")
-    print("Do you flee for your life or eat your head?")
+def cthulhu_room(name):
+    print("Here {} see the great evil Cthulhu.".format(name))
+    print("He, it, whatever stares at {} and you go insane.".format(name))
+    print("Do {} flee for {} life or eat {} head?".format(name, name + '\'s', name + '\'s'))
 
     next = input("> ")
 
     if "flee" in next:
-        start()
+        main()
     elif "head" in next:
         dead("Well that was tasty!")
     else:
-        cthulhu_room()
+        cthulhu_room(name)
 
 
 def dead(why):
@@ -83,20 +82,21 @@ def dead(why):
 
 
 ############################################################################
-def start():
+def main():
     # START the TextAdventure game
-    print("You are in a dark room.")
-    print("There is a door to your right and left.")
-    print("Which one do you take?")
+    name = input("Hi, what is your name? ")
+    print("{} is in a dark room.".format(name))
+    print("There is a door to {} right and left.".format(name + '\'s'))
+    print("Which one does {} take?".format(name))
 
     next = input("> ")
 
     if next == "left":
-        bear_room()
+        bear_room(name)
     elif next == "right":
-        cthulhu_room()
+        cthulhu_room(name)
     else:
-        dead("You stumble around the room until you starve.")
+        dead("{} stumbles around the room until {} starve.".format(name, name))
 
 if __name__ == '__main__':
-    start()
+    main()
