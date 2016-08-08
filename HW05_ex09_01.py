dffcf4e03cf101bb29ffc4d062c5fa6726afddb3
ghvik <vreddy@ischool.berkeley.edu>
2016-08-08 15:33:37 -0700

    Finished hw05 ex09_01, reading a file and printing long words

--- HW05_ex09_01.py
+++ HW05_ex09_01.py
@@ -7,11 +7,28 @@
 # Imports
 
 # Body
-
+def print_long_words():
+    document = read_file('words.txt')
+    for line in document:
+        for word in line.split():
+            if len(word) > 20:
+                print(word, "has more than 20 characters")
+                
+def read_file(file_name):
+    """Reads a file, handles any exceptions, and returns that file
+    
+    Parameter:
+    file_name -- the file to read"""
+    try:
+        document = open('words.txt', 'r')
+    except (IOError, PermissionError, IsADirectoryError) as e:
+        print("Something is wrong, please check the file name, or your permissions", e)
+    else:
+        return document
 
 ##############################################################################
 def main():
-    pass  # Call your functions here.
-
+    print_long_words()
+    
 if __name__ == '__main__':
     main()
